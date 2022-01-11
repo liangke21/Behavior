423526b138a50d2e58a65cd843350ddb93c0c201
liangke21 <l1396797522@foxmail.com>
2022-01-11 19:57:45 +0800

    实现资源文件跳转方向

--- viewpoint/src/main/java/com/liangke/viewpoint/behavior/GlobalBehavior.kt
+++ viewpoint/src/main/java/com/liangke/viewpoint/behavior/GlobalBehavior.kt
@@ -1,16 +1,30 @@
 package com.liangke.viewpoint.behavior
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import androidx.annotation.IntDef
+import androidx.annotation.RestrictTo
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
+import androidx.customview.view.AbsSavedState
+import com.liangke.viewpoint.R
 
 class GlobalBehavior<V : View> : CoordinatorLayout.Behavior<V> {
 
 
     constructor() : super()
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        val a = context?.obtainStyledAttributes(attrs, R.styleable.GlobalBehavior)
+        val direction = a?.getInt(R.styleable.GlobalBehavior_direction, -1)
+        Log.d(TAG, direction.toString())
+        a?.recycle()
+    }
 
 
     override fun onLayoutChild(parent: CoordinatorLayout, child: V, layoutDirection: Int): Boolean {
@@ -23,13 +37,16 @@ class GlobalBehavior<V : View> : CoordinatorLayout.Behavior<V> {
         ) {
             ViewCompat.setImportantForAccessibility(child, ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_YES)
         }
+
         parent.onLayoutChild(child, layoutDirection)
+
         ViewCompat.offsetLeftAndRight(child, 1030)
         return true
     }
 
 
     companion object {
+        private const val TAG = "GlobalBehavior"
 
         /**
          * 获取与view关联的GlobalBehavior
@@ -42,9 +59,42 @@ class GlobalBehavior<V : View> : CoordinatorLayout.Behavior<V> {
 
             val behavior = params.behavior
             require(behavior is CoordinatorLayout.Behavior<*>) { "该视图与 GlobalBehavior无关" }
-
+            @Direction val a = TOP_SHEET
+            Log.d(TAG, " a $a  b $BOTTOM_SHEET")
             return behavior as GlobalBehavior
         }
+
+        @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+        @IntDef(
+            TOP_SHEET,
+            BOTTOM_SHEET,
+            LEFT_SHEET,
+            RIGHT_SHEET
+        )
+        @Retention(AnnotationRetention.SOURCE)
+        annotation class Direction
+
+        /**
+         * 顶部工作表
+         */
+        const val TOP_SHEET = 1
+
+        /**
+         * 底部工作表
+         */
+        const val BOTTOM_SHEET = 2
+
+        /**
+         * 左部工作表
+         */
+        const val LEFT_SHEET = 3
+
+        /**
+         * 右部工作表
+         */
+        const val RIGHT_SHEET = 4
+
+
     }
 
 }
